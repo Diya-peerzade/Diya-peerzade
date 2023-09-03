aa8703ede0c7a44f5c8bacc66a3809d414270299
Diya-peerzade <143695790+Diya-peerzade@users.noreply.github.com>
2023-09-03 12:11:16 +0530

    Update Lab 1a.py

--- Lab 1a.py
+++ Lab 1a.py
@@ -17,17 +17,4 @@ print(30 * "*")
 percent = (total/max_marks) * 100
 print("Percentage of the Student:", round(percent))
 
-|-------------|
 
-
-
-import datetime
-name = input("Enter the name of the person: ")
-birth_year = int(input("Enter the year of birth of the person: "))
-today = datetime.date.today()
-year = today.year
-age = year - birth_year
-if age >= 60:
-print(name + "is a senior citizen")
-else:
-print(name + "is not a senior citizen")
